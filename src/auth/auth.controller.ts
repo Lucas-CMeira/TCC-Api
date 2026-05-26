73c57fd870266e6e52ae055e7a0b2dca751b3954
Lucas-CMeira <lucascmeira2005@gmail.com>
2026-05-26 20:10:42 -0300

    auth.controller - login

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,55 +1,61 @@
-
-/*
-
- - DEFINE ENDPOINTS
- - RECEBE AS REQUISIÇÕES;
- - CHAMA CAMADA RESPONSÁVEL POR APLICAR A LOGICA DE NEGÓCIO (SERVICE)
- - DEVOLVE UMA RESPOSTA PARA O CLIENTE;
-
-
- EXEMPLO DO HOSPITAL:
- Controller é a atendente do hospital, tem responsabilidade de receber as solicitacoes dos clientes e direcionar para os serviços adequados
-
- Mulher chega com seu filho no hospital -> ela quer consumir o serviço de pediatria
-
- A atendente (que seria o controller) -> faz perguntas (validacoes) e coleta todas informações iniciais necessarias e encaminha para o 
- doutor especialista no serviço desejado (no caso, o service de pediatria)
-
- depois disso a atendente sai de cena
-
- volta no fim do ciclo para dar tchau (feedback de resposta)
-
-*/
-import {FastifyRequest, FastifyReply} from "fastify"
+import { FastifyRequest, FastifyReply } from "fastify"
 import { AuthService } from "./auth.service"
+
 export class AuthController {
 
-    constructor(private authService : AuthService){
+    constructor(private authService: AuthService) {}
 
-    }
+    // Cadastro
+    async register(request: FastifyRequest, reply: FastifyReply) {
 
-    // Recebe as requisições do front e chama o authService para registrar um usuário
+        try {
 
-    async register (request: FastifyRequest, reply: FastifyReply) {
-        const {name, email , password } = request.body as {
-            name: string
-            email: string
-            password: string
+            const { name, email, password } = request.body as {
+                name: string
+                email: string
+                password: string
+            }
+
+            const result = await this.authService.register(
+                name,
+                email,
+                password
+            )
+
+            return reply.status(201).send(result)
+
+        } catch (error: any) {
+
+            return reply.status(400).send({
+                message: error.message
+            })
+
         }
-        const result = await this.authService.register(name, email, password)
-
-        return reply.status(201).send(result)
     }
 
+    // Login
     async login(request: FastifyRequest, reply: FastifyReply) {
 
-    const { email, password } = request.body as {
-        email: string
-        password: string
+        try {
+
+            const { email, password } = request.body as {
+                email: string
+                password: string
+            }
+
+            const result = await this.authService.login(
+                email,
+                password
+            )
+
+            return reply.status(200).send(result)
+
+        } catch (error: any) {
+
+            return reply.status(401).send({
+                message: error.message
+            })
+
+        }
     }
-
-    const result = await this.authService.login(email, password)
-
-    return reply.status(200).send(result)
-}
 }
